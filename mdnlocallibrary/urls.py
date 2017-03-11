6f6a8e42ae0b591538c4f691f697b1a814eaa40c
tombiasz <tomasz.m.nowacki@gmail.com>
2017-03-11 21:24:40 +0100

    add catalog urls and redirect app root to catalog root

--- mdnlocallibrary/urls.py
+++ mdnlocallibrary/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls import include
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^catalog/', include('catalog.urls')),
+    url(r'^$', RedirectView.as_view(url='/catalog/', permanent=True)),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
