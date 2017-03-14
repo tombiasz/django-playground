a1148061308cd656f65d20d0987560396e970afa
tombiasz <tomasz.m.nowacki@gmail.com>
2017-03-14 20:14:11 +0100

    add fields layout to AuthorAdmin

--- catalog/admin.py
+++ catalog/admin.py
@@ -6,6 +6,7 @@ from .models import Author, Book, BookInstance, Genre
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 
 @admin.register(Book)
