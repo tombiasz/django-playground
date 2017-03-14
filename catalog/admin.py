0e9eb6646c87a04fda6e3f09d955a41b2d19cc2e
tombiasz <tomasz.m.nowacki@gmail.com>
2017-03-14 20:22:39 +0100

    add list display to BookInstanceAdmin

--- catalog/admin.py
+++ catalog/admin.py
@@ -20,6 +20,7 @@ class BookAdmin(admin.ModelAdmin):
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
     list_filter = ('status', 'due_back')
     fieldsets = (
         (None, {
