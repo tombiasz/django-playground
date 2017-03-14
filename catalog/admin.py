a246d2433b826569b6a2267990d3de45d3317282
tombiasz <tomasz.m.nowacki@gmail.com>
2017-03-14 19:51:04 +0100

    register ModelAdmin class for Author model

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 from .models import Author, Book, BookInstance, Genre
 
 
-admin.site.register(Author)
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Book)
 admin.site.register(BookInstance)
 admin.site.register(Genre)
