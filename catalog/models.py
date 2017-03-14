a04740bc2f481a78a5a9a6c81786615acb5cf663
tombiasz <tomasz.m.nowacki@gmail.com>
2017-03-14 19:43:42 +0100

    rename BookInstance field

--- catalog/models.py
+++ catalog/models.py
@@ -38,7 +38,7 @@ class BookInstance(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
     book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
-    impriint = models.CharField(max_length=200)
+    imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
     status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='d', help_text='Book availability')
 
