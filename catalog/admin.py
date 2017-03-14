e3bf3257862aadda65b65237b9ead47ed52b8c4c
tombiasz <tomasz.m.nowacki@gmail.com>
2017-03-14 20:16:47 +0100

    add fieldsets to BookInstance

--- catalog/admin.py
+++ catalog/admin.py
@@ -17,6 +17,14 @@ class BookAdmin(admin.ModelAdmin):
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book','imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
 
 
 admin.site.register(Genre)
