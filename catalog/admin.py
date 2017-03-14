31132ad327d86b170d045284bff9f4a3b44d46fc
tombiasz <tomasz.m.nowacki@gmail.com>
2017-03-14 19:54:19 +0100

    register Book model as ModelAdmin

--- catalog/admin.py
+++ catalog/admin.py
@@ -7,6 +7,11 @@ from .models import Author, Book, BookInstance, Genre
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
 
-admin.site.register(Book)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(BookInstance)
 admin.site.register(Genre)
