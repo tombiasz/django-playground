782dea0d9e8e3b059f444818281129f1d95def25
tombiasz <tomasz.m.nowacki@gmail.com>
2017-03-14 20:25:29 +0100

    add Book inlines to AuthorAdmin

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,10 +3,14 @@ from django.contrib import admin
 from .models import Author, Book, BookInstance, Genre
 
 
+class BooksInline(admin.TabularInline):
+    model = Book
+
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
 
 
 class BooksInstanceInline(admin.TabularInline):
