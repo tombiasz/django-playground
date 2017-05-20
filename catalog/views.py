bfa77732948d8f1810bc212bfa4e1e120880d3f9
tombiasz <tomasz.m.nowacki@gmail.com>
2017-05-20 18:47:37 +0200

    add genries count to index page

--- catalog/views.py
+++ catalog/views.py
@@ -8,14 +8,16 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.all().count()
+    num_genries = Genre.objects.all().count()
 
     return render(
         request,
         'index.html',
-        context = {
+        context={
             'num_books': num_books,
             'num_instances': num_instances,
             'num_instances_available': num_instances_available,
             'num_authors': num_authors,
+            'num_genries': num_genries,
         },
     )
