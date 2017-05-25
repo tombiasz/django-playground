3089d04fe0c91876c85a74a6cc2565415d516e95
tombiasz <tomasz.m.nowacki@gmail.com>
2017-05-25 21:21:32 +0200

    link from menu to book and author list pages

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^books/$', views.BookListView.as_view()),
+    url(r'^books/$', views.BookListView.as_view(), name='book-list'),
     url(r'^books/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
-    url(r'^authors/$', views.AuthorListView.as_view()),
+    url(r'^authors/$', views.AuthorListView.as_view(), name='author-list'),
     url(r'^authors/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
