b76534ea09a66d94c503caa58bb69f03de3628f4
tombiasz <tomasz.m.nowacki@gmail.com>
2017-09-07 17:57:55 +0200

    rename base app to config

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,4 @@
-"""mdnlocallibrary URL Configuration
+"""django-playground URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
