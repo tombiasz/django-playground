1e6bb4aa8c7e1c154076e6c8430247ca128d6151
tombiasz <tomasz.m.nowacki@gmail.com>
2017-09-07 13:25:53 +0200

    add label to renewal date field

--- catalog/forms.py
+++ catalog/forms.py
@@ -6,7 +6,10 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class RenewBookForm(forms.Form):
-    renewal_date = forms.DateField(help_text="Enter a date between now and 4 weeks (default 3)")
+    renewal_date = forms.DateField(
+        label="Renewal date",
+        help_text="Enter a date between now and 4 weeks (default 3)"
+    )
 
     def clean_renewal_date(self):
         data = self.cleaned_data['renewal_date']
