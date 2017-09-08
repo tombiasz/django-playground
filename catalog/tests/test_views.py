834dbe1a63b200ac479b9db3a31c16d890bf4ffe
tombiasz <tomasz.m.nowacki@gmail.com>
2017-09-08 19:32:21 +0200

    add tests for books list view

--- catalog/tests/test_views.py
+++ catalog/tests/test_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from catalog.models import Author
+from catalog.models import Author, Book
 from django.core.urlresolvers import reverse
 
 
@@ -34,3 +34,34 @@ class AuthorListViewTest(TestCase):
         self.assertTrue('is_paginated' in resp.context)
         self.assertTrue(resp.context['is_paginated'] == True)
         self.assertTrue(len(resp.context['author_list']) == 10)
+
+
+class BookListViewTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        number_of_books = 13
+        for book_num in range(number_of_books):
+            Book.objects.create(
+                title='Title {}'.format(book_num),
+            )
+
+    def test_view_url_returns_http_ok(self):
+        resp = self.client.get('/catalog/books/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_url_is_accessible_by_url_name(self):
+        resp = self.client.get(reverse('book-list'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('book-list'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'catalog/book_list.html')
+
+    def test_pagination_is_ten(self):
+        resp = self.client.get(reverse('book-list'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('is_paginated' in resp.context)
+        self.assertTrue(resp.context['is_paginated'] == True)
+        self.assertTrue(len(resp.context['book_list']) == 10)
